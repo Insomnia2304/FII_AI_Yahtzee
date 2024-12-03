d851d76f265eae6dd3e82db1ef4aed7ae5383e39
Sorin <sorinl2002@yahoo.com>
2024-12-03 20:45:10 +0200

    redone rewarding system
    redone rewarding system so that it covers more scenarios;
    rewrote some other functions to work with the new get_reward()

--- q_learning.py
+++ q_learning.py
@@ -8,25 +8,29 @@ import matplotlib.pyplot as plt
 import pickle
 
 TURNS = 13
-ALPHA = 0.2 # learning rate
-DECAY_RATE = 0.98
-DISCOUNT = 0.7 # discount factor
-EXPLORATION_CHANCE = 0.999
+ALPHA = 0.05
+DECAY_RATE = 0.95
+DISCOUNT = 0.9
+EXPLORATION_CHANCE = 0.9
 EXPLORATION_CHANCE_DECAY = 0.9999
+MIN_EXPLORATION_CHANCE = 0.05
 
 current_turn = 0
 dice, keep_dice, state, dice_rolls = set_initial_state()
 
-def decay_exploration_rate():
-    global EXPLORATION_CHANCE, EXPLORATION_CHANCE_DECAY
-    EXPLORATION_CHANCE *= EXPLORATION_CHANCE_DECAY
 
-def int_to_tuple(choice: int) -> tuple[int,...]:
+def decay_exploration_rate():
+    global EXPLORATION_CHANCE
+    EXPLORATION_CHANCE = max(MIN_EXPLORATION_CHANCE, EXPLORATION_CHANCE * EXPLORATION_CHANCE_DECAY)
+
+
+def int_to_tuple(choice: int) -> tuple[int, ...]:
     return tuple(int(x) for x in f"{choice:05b}")
+
 
 def init_q_table() -> dict:
     dice_combinations = [(a, b, c, d, e) for a in range(1, 7) for b in range(a, 7) for c in range(b, 7) for d in range(c, 7) for e in range(d, 7)]
-    choices = [(a, b, c, d, e) for a in range(2) for b in range(2) for c in range(2) for d in range(2) for e in range(2)][1:] # exclude 0,0,0,0,0 since it basically means rolling all dice
+    choices = [(a, b, c, d, e) for a in range(2) for b in range(2) for c in range(2) for d in range(2) for e in range(2)][1:]  # exclude 0,0,0,0,0 since it basically means rolling all dice
 
     Q = {}
     for comb in dice_combinations:
@@ -37,31 +41,28 @@ def init_q_table() -> dict:
 
         for row in SCORE_ROWS:
             Q[comb][row] = 0.01
-    
+
     return Q
-    
-def choose_action(sorted_dice, remaining_rolls, Q: dict, state: dict, EXPLORATION_CHANCE = 0.0) -> tuple[int,...] | int:
+
+
+def choose_action(sorted_dice, remaining_rolls, Q: dict, state: dict, exploration_chance=0.0) -> tuple[int, ...] | int:
     if remaining_rolls == 0:
         available_actions = [row for row in SCORE_ROWS if state['points_table'][1][row] == -1]
-        if np.random.rand() < EXPLORATION_CHANCE:
+        if np.random.rand() < exploration_chance:
             return random.choice(available_actions)
         else:
             return max(available_actions, key=lambda action: Q[sorted_dice][action])
     else:
         available_actions_dice = [int_to_tuple(choice) for choice in range(1, 31)]
-        available_actions_score = [row for row in SCORE_ROWS if state['points_table'][1][row] == -1]
-        available_actions = available_actions_dice + available_actions_score
-        if np.random.rand() < EXPLORATION_CHANCE:
-            return random.choice(available_actions)
+        if np.random.rand() < exploration_chance:
+            return random.choice(available_actions_dice)
         else:
-            return max(available_actions, key=lambda action: Q[sorted_dice][action])
+            return max(available_actions_dice, key=lambda action: Q[sorted_dice][action])
 
 
-def update_q_value(old_state: tuple[int,...], new_state: tuple[int,...], action: tuple[int,...] | int, score = -1):
+def update_q_value(old_state: tuple[int, ...], new_state: tuple[int, ...], action: tuple[int, ...] | int, reward: float):
     global Q, DISCOUNT, ALPHA
-    reward = q_utils.get_reward(new_state, score)
     Q[old_state][action] += ALPHA * (reward + DISCOUNT * max(Q[new_state].values()) - Q[old_state][action])
-    pass
 
 
 Q = init_q_table()
@@ -69,25 +70,31 @@ Q = init_q_table()
 
 def episode():
     global current_turn, dice, keep_dice, state, dice_rolls, EXPLORATION_CHANCE
+    current_turn = 0
+    dice, keep_dice, state, dice_rolls = set_initial_state()
+
     while current_turn < TURNS:
         remaining_rolls = 2
         dice = dice_utils.dice_roll(len(dice))
         sorted_dice = sorted(dice + keep_dice)
         action = choose_action(tuple(sorted_dice), remaining_rolls, Q, state, EXPLORATION_CHANCE)
+
         while isinstance(action, tuple) and remaining_rolls > 0:
-            #print("Rolling dice",action)
             sorted_dice = sorted(dice + keep_dice)
             dice, keep_dice = dice_utils.choose_dice_q(list(action), sorted_dice)
             new_sorted_dice = sorted(dice + keep_dice)
-            update_q_value(tuple(sorted_dice), tuple(new_sorted_dice), action)
+            reward = q_utils.get_reward(state, tuple(new_sorted_dice), -1, state['points_table'][1], state['points_table'][1], remaining_rolls)
+            update_q_value(tuple(sorted_dice), tuple(new_sorted_dice), action, reward)
             remaining_rolls -= 1
             action = choose_action(tuple(new_sorted_dice), remaining_rolls, Q, state, EXPLORATION_CHANCE)
         else:
-            #print("Updating score")
             score = q_utils.update_score(state, action, 1, dice, keep_dice)
             sorted_dice = sorted(dice + keep_dice)
-            update_q_value(tuple(sorted_dice), tuple(sorted_dice), action, score)
+            reward = q_utils.get_reward(state, tuple(sorted_dice), action, state['points_table'][1], state['points_table'][1], remaining_rolls, score)
+            update_q_value(tuple(sorted_dice), tuple(sorted_dice), action, reward)
+
         current_turn += 1
+
 
 scores = []
 
@@ -99,15 +106,12 @@ if __name__ == "__main__":
             break
         print(f"Episode {i} completed")
         scores.append(state['points_table'][1][SCORE_ROW])
-        current_turn = 0
-        dice, keep_dice, state, dice_rolls = set_initial_state()
 
     plt.plot(scores)
     window_size = 500
-    rolling_mean = np.convolve(scores, np.ones(window_size)/window_size, mode='valid')
+    rolling_mean = np.convolve(scores, np.ones(window_size) / window_size, mode='valid')
     plt.plot(range(len(rolling_mean)), rolling_mean)
     plt.show()
 
-
     with open('q_table.pkl', 'wb') as file:
-        pickle.dump(Q,file)
+        pickle.dump(Q, file)
--- utils/q_utils.py
+++ utils/q_utils.py
@@ -1,6 +1,7 @@
 import math
 
 from constants.constants import *
+from q_learning import TURNS
 from utils import dice_utils
 import numpy as np
 
@@ -15,7 +16,7 @@ def update_score(state, row, player, dice, keep_dice):
         if state['points_table'][player][YAHTZEE_ROW] != 0:
             state['points_table'][player][YAHTZEE_ROW] += 100
             state['points_table'][player][SCORE_ROW] += 100
-        if state['points_table'][player][total_dice[0]-1] != -1:
+        if state['points_table'][player][total_dice[0] - 1] != -1:
             upper_side_completed = True
 
     score = dice_utils.validate_choice(total_dice, row, joker=joker, upper_side_completed=upper_side_completed)
@@ -31,20 +32,50 @@ def update_score(state, row, player, dice, keep_dice):
 
     return score
 
-def get_reward(dice: tuple[int,...], score=-1) -> float:
+
+def get_reward(
+        state: dict,
+        dice: tuple[int, ...],
+        action: int,
+        old_scores: dict,
+        new_scores: dict,
+        remaining_rolls: int,
+        score=-1
+) -> float:
+
     if score != -1:
-        return score
+        row = action - 31
+        if score == 0:
+            if row in ['YAHTZEE_ROW', 'LARGE_STRAIGHT_ROW', 'FULL_HOUSE_ROW']:
+                if state['turn'] <= TURNS // 2:
+                    return -20
+                else:
+                    potential_score = dice_utils.validate_choice(list(dice), row)
+                    return -5 if potential_score == 0 else -10
+            else:
+                return -2
 
-    if score == 0:
-        return -5
+        reward = score
+        if row in ['YAHTZEE_ROW', 'LARGE_STRAIGHT_ROW', 'FULL_HOUSE_ROW']:
+            reward += 10
+        if row <= 5 and state['points_table'][1][SUM_ROW] + score >= 63:
+            reward += 15
+        return reward
 
-    potential_scores = []
-    for row in SCORE_ROWS:
-        potential_scores.append(dice_utils.validate_choice(list(dice), row))
+    incomplete_rows = [row for row in SCORE_ROWS if state['points_table'][1][row] == -1]
+    old_avg_score = np.mean([old_scores[row] for row in incomplete_rows])
+    new_avg_score = np.mean([new_scores[row] for row in incomplete_rows])
+    reward = new_avg_score - old_avg_score
 
-    reward = np.mean(potential_scores)
-    if math.isnan(reward):
-        print(potential_scores)
-        print("NAN")
+    if remaining_rolls == 0:
+        reward -= 10 if new_avg_score < old_avg_score else 5
+    else:
+        reward += 10 if new_avg_score > old_avg_score else -5
+
+    for row in incomplete_rows:
+        if row in ['YAHTZEE_ROW', 'LARGE_STRAIGHT_ROW', 'FULL_HOUSE_ROW']:
+            potential_score = dice_utils.validate_choice(list(dice), row)
+            if potential_score > 0:
+                reward += 10
 
     return reward
